a608a4681fbbdf61920313b0ba291562abd471e5
liudf <liudf@autoxing.com>
2023-11-15 20:11:17 +0800

    tweak

--- src/laser_converter.cpp
+++ src/laser_converter.cpp
@@ -7,8 +7,8 @@ inline int16_t Util_degreeToI16(float degree)
 
 LaserConverter::LaserConverter() : m_nh("~")
 {
-    m_laserSub = m_nh.subscribe("/scan_raw", 1000, &LaserConverter::_laserScanCallback, this);
-    m_laserPub = m_nh.advertise<laser_scan_converter::AxLaserScan>("/ax_laser_scan2", 10);
+    m_laserSub = m_nh.subscribe("/scan_raw", 10, &LaserConverter::_laserScanCallback, this);
+    m_laserPub = m_nh.advertise<laser_scan_converter::AxLaserScan>("/ax_laser_scan", 10);
 }
 
 void LaserConverter::_laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
