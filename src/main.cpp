4527f86b14b31c559cc57c9b62294e65c5332801
liudf <liudf@autoxing.com>
2023-11-15 17:58:26 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "laser_scan_converter");
+
+    // SensorHubClient client;
+    // return client.run();
+    return 0;
+}
